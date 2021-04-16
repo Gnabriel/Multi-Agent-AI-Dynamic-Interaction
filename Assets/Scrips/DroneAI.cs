feace6e28e052a32392c9ba4904827d856cec2cc
Gnabriel <elleb@kth.se>
2021-04-16 11:33:55 +0200

    Added backbones to HRVO collision avoidance

--- Assets/Scrips/DroneAI.cs
+++ Assets/Scrips/DroneAI.cs
@@ -66,12 +66,135 @@ public class DroneAI : MonoBehaviour
         Vector3 relVect = my_goal_object.transform.position - transform.position;
 
         m_Drone.Move_vect(relVect);
-
-        
-
     }
 
- 
+
+    public void CollisionAvoidance(List<GameObject> agents, List<Vector3> obstacles)
+    {
+        // Collision avoidance using Hybrid Reciprocal Velocity Obstacles (HRVO).
+        // Source: https://ieeexplore.ieee.org/abstract/document/5746538.
+        // Inputs:
+        // - agents: list of robots to avoid collision with that is assumed to follow the same HRVO collision avoidance procedure.
+        // - obstacles: list of other obstacles to avoid collision with.
+
+        bool vo;                                                                            // Velocity Obstacle (VO) object.
+        bool rvo;                                                                           // Reciprocal Velocity Obstacle (RVO) object.
+        bool hrvo;                                                                          // Hybrid Reciprocal Velocity Obstacle (HRVO) object.
+        List<bool> agent_i_hrvo_list;                                                       // List of HRVO*_Ai|Aj objects for agent_i (Ai) with every other agent_j (Aj).
+        List<bool> agent_i_vo_list;                                                         // List of VO*_Ai|Oj objects for agent_i (Ai) with every other obstacle_j (Oj).
+        Vector3 cl;                                                                         // Center line of a HRVO.
+        Vector3 agent_i_pos;                                                                // Current position of agent_i.
+        Vector3 agent_i_v;                                                                  // Current velocity of agent_i.
+        Vector3 agent_i_v_pref;                                                             // Preferred velocity of agent_i.
+        Vector3 agent_i_v_new;                                                              // New velocity of agent_i.
+        Vector3 agent_j_pos;                                                                // Current position of agent_j.
+        Vector3 agent_j_v;                                                                  // Current velocity of agent_j.
+
+        foreach (GameObject agent_i in agents)                                                                      // Loop over each agent_i.
+        {
+            agent_i_pos = agent_i.transform.position;
+            agent_i_v = agent_i.GetComponent<Rigidbody>().velocity;
+            agent_i_hrvo_list = new List<bool>();
+            agent_i_vo_list = new List<bool>();
+
+            foreach (GameObject agent_j in agents)                                                                  // Loop over each agent_j.
+            {
+                if (agent_i == agent_j)
+                {
+                    break;                                                                                          // Skip if agent_j is agent_i.
+                }
+                agent_j_pos = agent_j.transform.position;
+                agent_j_v = agent_j.GetComponent<Rigidbody>().velocity;
+
+                vo = VelocityObstacle(agent_i_pos, agent_i_v, agent_j_pos, agent_j_v);                              // Construct VO_Ai|Aj.
+                (rvo, cl) = ReciprocalVelocityObstacle(agent_i_pos, agent_i_v, agent_j_pos, agent_j_v);             // Construct RVO_Ai|Aj.
+
+                Vector3 v_a = new Vector3(0, 0, 0);                                                                 // TODO: What is v_a? Is it v_a_i or v_a_j or something else?
+                if (CheckToRight(v_a, cl))
+                {
+                    hrvo = HybridReciprocalVelocityObstacle("left", vo, rvo);                                       // Construct HRVO_Ai|Aj using left edge of VO_Ai|Aj.
+                }
+                else
+                {
+                    hrvo = HybridReciprocalVelocityObstacle("right", vo, rvo);                                      // Construct HRVO_Ai|Aj using right edge of VO_Ai|Aj.
+                }
+
+                hrvo = HybridReciprocalVelocityObstacleStar(hrvo);                                                  // Expand HRVO_Ai|Aj to HRVO*_Ai|Aj.
+                agent_i_hrvo_list.Add(hrvo);                                                                        // Save the HRVO*_Ai|Aj in a list for this agent_i. 
+            }
+
+            foreach (Vector3 obstacle_j_pos in obstacles)                                                           // Loop over each obstacle position.
+            {
+                Vector3 obstacle_j_vel = new Vector3(0, 0, 0);                                                      // We only have static obstacles, i.e velocity zero.
+                vo = VelocityObstacle(agent_i_pos, agent_i_v, obstacle_j_pos, obstacle_j_vel);                      // Construct VO_Ai|Oj.
+                vo = VelocityObstacleStar(vo);                                                                      // Construct VO*_Ai|Oj.
+                agent_i_vo_list.Add(vo);                                                                            // Save the VO*_Ai|Oj in a list for this agent_i. 
+            }
+
+            hrvo = false;  // Use agent_i_hrvo_list and agent_i_vo_list.                                            // Construct HRVO*_Ai from every HRVO*_Ai|Aj and VO*_Ai|Oj.
+            agent_i_v_pref = GetPreferredVelocity();
+            agent_i_v_new = GetNewVelocity();
+
+        }
+    }
+
+
+    public bool VelocityObstacle(Vector3 agent1_pos, Vector3 agent1_vel, Vector3 agent2_pos, Vector3 agent2_vel)
+    {
+        return false;
+    }
+
+
+    public bool VelocityObstacleStar(bool vo)
+    {
+        return false;
+    }
+
+
+    public (bool, Vector3) ReciprocalVelocityObstacle(Vector3 agent1_pos, Vector3 agent1_vel, Vector3 agent2_pos, Vector3 agent2_vel)
+    {
+        return (false, new Vector3(0, 0, 0));
+    }
+
+
+    public bool HybridReciprocalVelocityObstacle(string side, bool vo, bool rvo)
+    {
+        return false;
+    }
+
+
+    public bool HybridReciprocalVelocityObstacleStar(bool hrvo)
+    {
+        return false;
+    }
+
+
+    public Vector3 GetPreferredVelocity()
+    {
+        return new Vector3(0, 0, 0);
+    }
+
+
+    public Vector3 GetNewVelocity()
+    {
+        return new Vector3(0, 0, 0);
+    }
+
+
+    public bool CheckToRight(Vector3 this_vector, Vector3 other_vector)
+    {
+        // Checks if this_vector is to the right of other_vector.
+        // Output: true if to the right, false if to the left.
+        if (Vector3.Dot(other_vector, this_vector) > 0)                                                             // TODO: Is this correct or should it be the other way around?
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
